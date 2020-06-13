4628ba293cfc978dbfd9ac753ec7038c49e4b072
Yahi09 <valenciayahaira262@gmail.com>
2020-06-12 22:24:37 -0500

    version 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+/*este es el componente padre*/
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FUNDAMENTOS';
+  name = 'Ines';//variable en el componente nueva y vale ines y se envia la variable nombre que es nombre
+  onSaludar(mensaje){/*funcion, que recibira un mensaje para mostrarlo en consola*/
+    console.log(mensaje);//aqui recibe lo que envian del componente hijo y lo esta imprimiendo
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+//import { AppRoutingModule } from './app-routing.module';
+//debes de borrar esto y en el imports
 import { AppComponent } from './app.component';
+import {SaludarComponent } from './componentes/saludar.component';
+//el import de saludar viene de la clase que se va a usar
+//aqui se usa esa clase que se hizo publica en el ts (export)
 
 @NgModule({
   declarations: [
-    AppComponent
+    //aqui se declaran los componentes 
+    //que se van a usar para todo el proyecto
+    AppComponent,
+    SaludarComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
